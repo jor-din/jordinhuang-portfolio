58311c6aa28a13200b2ca8887753895fb2d365a6
jordinhuang <jordinhuang@gmail.com>
2022-09-21 17:16:39 -0400

    add burger menu for mobile/tablet size

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,12 +4,12 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Link from '@mui/material/Link'
-
+import {slide as Menu} from 'react-burger-menu'
 
 const NavBar = () => {
   return (
     <header className="navbar">
-      <Box sx={{ flexGrow: 1}}>
+      <Box sx={{ flexGrow: 1}} className='inline-nav'>
         <AppBar position="static" style={{background: '#FFF8F1', height: '7vh'}} >
           <Toolbar variant='dense' className="nav-buttons">            
             <Link className='nav-link' underline="none" style={{color:'#8B72BE', fontSize: '1.5rem'}} href="/">
@@ -27,6 +27,12 @@ const NavBar = () => {
           </Toolbar>
         </AppBar>
       </Box>
+      <Menu className="mobile-nav" noOverlay>
+        <a className="menu-item" href="/">Home</a>
+        <a className="menu-item" href="/about">About</a>
+        <a className="menu-item" href="/projects">Works</a>
+        <a className="menu-item" href='https://docs.google.com/document/d/11kDx6b9d6JmSWjUq1MRRr2nLYMHx7Y0yPMjM8dVmd6U/edit?usp=sharing' target='_blank' rel='noopener noreferrer'>Resume</a>
+      </Menu>
     </header>
   );
 };
